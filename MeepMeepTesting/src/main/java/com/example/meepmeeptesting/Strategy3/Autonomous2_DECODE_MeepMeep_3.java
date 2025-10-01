2870aeecc15a417b4fa9f7b42545c84de9ad1577
STEAMachines <steamachines_42@outlook.com>
2025-10-01 22:07:40 +0700

    UPDATE, 10/1/2025 10.07 PM.

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Strategy3/Autonomous2_DECODE_MeepMeep_3.java
@@ -0,0 +1,22 @@
+package com.example.meepmeeptesting.Strategy3;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class Autonomous2_DECODE_MeepMeep_3 {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+        RoadRunnerBotEntity STEAMachines_bot = new DefaultBotBuilder(meepMeep)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+        STEAMachines_bot.runAction(STEAMachines_bot.getDrive().actionBuilder(new Pose2d(60, -15, 0))
+                .build());
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(STEAMachines_bot)
+                .start();
+    }
+}
