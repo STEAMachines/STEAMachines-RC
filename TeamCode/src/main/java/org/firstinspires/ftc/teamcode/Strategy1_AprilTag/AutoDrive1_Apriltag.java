eb03d73e8832d27430b6eec398c7a6b2045723a0
STEAMachines <steamachines_42@outlook.com>
2025-10-01 21:09:02 +0700

    UPDATE, 10/1/2025.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Strategy1_AprilTag/AutoDrive1_Apriltag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Strategy1_AprilTag/AutoDrive1_Apriltag.java
@@ -3,16 +3,33 @@ package org.firstinspires.ftc.teamcode.Strategy1_AprilTag;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
 
 
 @Autonomous(name="AutoDrive1_AprilTag", group="STEAMachines_DECODE")
 public class AutoDrive1_Apriltag extends LinearOpMode {
+    final double DESIRED_DISTANCE = 12.0;
+    final double SPEED_GAIN = 0.02;
+    final double TURN_GAIN = 0.01;
+    final double MAX_AUTO_SPEED = 1.0;
+    final double MAX_AUTO_TURN = 0.5;
+
     private DcMotor leftDrive;
     private DcMotor rightDrive;
     private DcMotor shootMotors;
-    AprilTagProcessor aprilTag;
+    private AprilTagProcessor aprilTag;
+    private VisionPortal visionPortal;
+    private AprilTagDetection desiredTag;
+    private static final boolean USE_WEBCAM = true;
+    private static final int DESIRED_TAG_ID = 20;
 
     public void runOpMode() {
         leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
@@ -22,11 +39,61 @@ public class AutoDrive1_Apriltag extends LinearOpMode {
         boolean isShooting = false;
         initializeAprilTag();
         waitForStart();
+        displayWebcamVision();
         autoDrive();
     }
     public void initializeAprilTag() {
         aprilTag = new AprilTagProcessor.Builder().build();
-        aprilTag.setDecimation(3);
+//        aprilTag.setDecimation(3); //If wanna use decimation for down-sampling images or decrease the amount of fps to create smaller images
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        if(USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam1"));
+        }
+        else{
+            builder.setCamera(BuiltinCameraDirection.BACK);
+        }
+        builder.addProcessor(aprilTag);
+        visionPortal = builder.build();
+    }
+    public void displayWebcamVision() {
+        boolean targetFound = true;
+        double drive = 0;
+        double turn = 0;
+        while(opModeIsActive()) {
+            List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+            for(AprilTagDetection detection:currentDetections) {
+                if(detection.metadata != null) {
+                    if(DESIRED_TAG_ID < 0 || (detection.id == DESIRED_TAG_ID)) {
+                        targetFound = true;
+                        desiredTag = detection;
+                        break;
+                    }
+                    else {
+                        telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
+                    }
+                }
+                else {
+                    telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
+                }
+                telemetry.addData("ID", detection.id);
+                telemetry.addData("Range", detection.ftcPose.range);
+                telemetry.addData("Bearing", detection.ftcPose.bearing);
+
+            }
+            if(targetFound) {
+                double rangeError = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
+                double headingError = desiredTag.ftcPose.bearing;
+                drive = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+                turn = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN);
+                telemetry.addData("Found", "ID %d (%s)", desiredTag.id, desiredTag.metadata.name);
+                telemetry.addData("Range",  "%5.1f inches", desiredTag.ftcPose.range);
+                telemetry.addData("Bearing","%3.0f degrees", desiredTag.ftcPose.bearing);
+            }
+            else {
+                targetFound = false;
+            }
+            telemetry.update();
+        }
     }
     public void autoDrive() {
         leftDrive.setPower(0.5);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Strategy1_AprilTag/AutoDrive3_AprilTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Strategy1_AprilTag/AutoDrive3_AprilTag.java
@@ -3,12 +3,31 @@ package org.firstinspires.ftc.teamcode.Strategy1_AprilTag;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
 
 @Autonomous(name="AutoDrive3_AprilTag", group="STEAMachines_DECODE")
 public class AutoDrive3_AprilTag extends LinearOpMode {
+    final double DESIRED_DISTANCE = 12.0;
+    final double SPEED_GAIN = 0.01;
+    final double TURN_GAIN = 0.02;
+    final double MAX_AUTO_SPEED = 1.0;
+    final double MAX_AUTO_TURN = 0.5;
     private DcMotor leftDrive;
     private DcMotor rightDrive;
     private DcMotor shootMotors;
+    private static final boolean USE_WEBCAM = true;
+    private static final int DESIRED_TAG_ID = 20;
+    private AprilTagProcessor aprilTag;
+    private VisionPortal visionPortal;
+    private AprilTagDetection desiredTag;
 
     public void runOpMode() {
         leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
@@ -18,10 +37,60 @@ public class AutoDrive3_AprilTag extends LinearOpMode {
         boolean isShooting = false;
         initializeAprilTag();
         waitForStart();
+        displayWebcamVision();
         autoDrive();
     }
     public void initializeAprilTag() {
+        aprilTag = new AprilTagProcessor.Builder().build();
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        if(USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam1"));
+        }
+        else {
+            builder.setCamera(BuiltinCameraDirection.BACK);
+        }
+        builder.addProcessor(aprilTag);
+        visionPortal = builder.build();
+    }
+    public void displayWebcamVision() {
+        boolean targetFound = true;
+        double drive = 0;
+        double turn = 0;
+        while(opModeIsActive()) {
+            List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+            for(AprilTagDetection detection:currentDetections) {
+                if(detection.metadata != null) {
+                    if(DESIRED_TAG_ID < 0 || (detection.id == DESIRED_TAG_ID)) {
+                        targetFound = true;
+                        desiredTag = detection;
+                        break;
+                    }
+                    else {
+                        telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
+                    }
+                }
+                else {
+                    telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
+                }
+                telemetry.addData("ID", detection.id);
+                telemetry.addData("Range", detection.ftcPose.range);
+                telemetry.addData("Bearing", detection.ftcPose.bearing);
 
+            }
+            if(targetFound) {
+                double rangeError = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
+                double headingError = desiredTag.ftcPose.bearing;
+                drive = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+                turn = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN);
+                telemetry.addData("Found", "ID %d (%s)", desiredTag.id, desiredTag.metadata.name);
+                telemetry.addData("Range",  "%5.1f inches", desiredTag.ftcPose.range);
+                telemetry.addData("Bearing","%3.0f degrees", desiredTag.ftcPose.bearing);
+            }
+            else {
+                targetFound = false;
+            }
+            telemetry.update();
+        }
     }
     public void autoDrive() {
 
